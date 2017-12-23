d3ea8130637d7e2f8ec22ba4d9a821e33f6badc9
Gigasaur <wrmagroganiii@gmail.com>
2017-12-22 22:02:43 -0700

    boundry checking

--- snowflake.py
+++ snowflake.py
@@ -6,27 +6,44 @@ class Particle:
         self.y = random.randint(0, m)
 
     def move(self):
-        self.x = (self.x + random.randint(-1, 1))%n
-        self.y = (self.y + random.randint(-1, 1))%m
+        self.x = (self.x + random.randint(-1, 1))
+        self.y = (self.y + random.randint(-1, 1))
 
     def draw(self):
         pass
 
 class Board:
     def __init__(self, n, m, N):
-        self.particles = [Particle(n, m)]*N
+        self.n = n
+        self.m = m
+        self.particles = [Particle(n, m) for i in range(N)]
 
     def draw(self):
-        map(lambda x: x.draw(), self.particles)
+        for p in self.particles:
+            p.draw()
 
     def step(self):
-        map(lambda x: x.move(), self.particles)
+        for p in self.particles:
+            p.move()
+            # boundary check
+            p.x %= self.n
+            p.y %= self.m
+
+    def print_state(self):
+        for p in self.particles:
+            print("{}, {}".format(p.x, p.y))
+
+    def iterate(self):
+        self.step()
+        self.draw()
 
 def main():
-    b = Board(2048, 2048, 2048)
+    b = Board(10, 10, 5)
+    b.print_state()
     for i in range(2048):
-        b.step()
-    pass
+        b.iterate()
+    print("")
+    b.print_state()
 
 if __name__ == "__main__":
     main()
